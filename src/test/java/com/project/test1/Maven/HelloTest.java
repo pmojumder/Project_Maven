55904bd920cfe64abe5ac8dfeb43342949150813
pmojumde <pmojumde@LIN83001100.corp.capgemini.com>
2020-04-25 23:57:02 +0530

    Validate

---
+++ src/test/java/com/project/test1/Maven/HelloTest.java
@@ -0,0 +1,13 @@
+package com.project.test1.Maven;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class HelloTest {
+
+	@Test
+	public void validate()
+	{
+		System.out.println("Valiadate");
+	}
+}
